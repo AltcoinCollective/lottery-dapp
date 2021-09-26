1b9b53b05df25b39002ba17d67b55a91b75c2a23
unknown <kehinde.oke@ikooba.com>
2021-09-26 20:01:38 +0100

    new contract redployd

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,40 +1,28 @@
-import React from 'react'
+import React, {useMemo, ReactNode} from 'react'
 import ReactDOM from 'react-dom'
-// import useActiveWeb3React from './hooks/useActiveWeb3React'
-// import { BLOCKED_ADDRESSES } from './config/constants'
-// import ListsUpdater from './state/lists/updater'
-// import MulticallUpdater from './state/multicall/updater'
-// import TransactionUpdater from './state/transactions/updater'
+import useActiveWeb3React from './hooks/useActiveWeb3React'
+import { BLOCKED_ADDRESSES } from './config/constants'
 import App from './App'
 import Providers from './Providers'
 
-// function Updaters() {
-//   return (
-//     <>
-//       <ListsUpdater />
-//       <TransactionUpdater />
-//       <MulticallUpdater />
-//     </>
-//   )
-// }
 
-// function Blocklist({ children }: { children: ReactNode }) {
-//   const { account } = useActiveWeb3React()
-//   const blocked: boolean = useMemo(() => Boolean(account && BLOCKED_ADDRESSES.indexOf(account) !== -1), [account])
-//   if (blocked) {
-//     return <div>Blocked address</div>
-//   }
-//   return <>{children}</>
-// }
+function Blocklist({ children }: { children: ReactNode }) {
+  const { account } = useActiveWeb3React()
+  const blocked: boolean = useMemo(() => Boolean(account && BLOCKED_ADDRESSES.indexOf(account) !== -1), [account])
+  if (blocked) {
+    return <div>Blocked address</div>
+  }
+  return <>{children}</>
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Blocklist> */}
+    <Blocklist>
       <Providers>
         {/* <Updaters /> */}
         <App />
       </Providers>
-    {/* </Blocklist> */}
+    </Blocklist>
   </React.StrictMode>,
   document.getElementById('root'),
 )
